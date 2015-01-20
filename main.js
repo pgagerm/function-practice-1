1841a1114c9edd25524f566f21c3a15c35a0b1e8
Michael Germany <mike@rickgermany.com>
2015-01-20 15:50:47 -0700

    5 of 10 exercises completed

--- main.js
+++ main.js
@@ -1,10 +1,54 @@
 // Function Practice #1
 
-var tripleFive = function() {
-	for (var i = 0; i < 3; i++) {
-		console.log("Five!");
-	}
+// var tripleFive = function() {
+// 	for (var i = 0; i < 3; i++) {
+// 		console.log("Five!");
+// 	}
+// }
+
+// tripleFive();
+
+
+
+// var lastLetter = function(w) {
+// 	var input = w;
+// 	for (var i = 0; i <= input; i++) {
+// 		return input.substring(i);
+// 	}
+// }
+
+// lastLetter("super");
+
+
+
+// var lastLetter = function(w) {
+// 		return w.charAt(w.length - 1);
+// }
+
+// (lastLetter("super"));
+
+
+
+// var square = function(n) {
+// 	n = n * n;
+// 	return n;
+// }
+
+// console.log(square(3));
+
+
+
+// var negate = function(n) {
+// 	n = -n;
+// 	return n;
+// }
+
+// console.log(negate(-8));
+
+
+
+var toArray = function(a, b, c) {
+	return ("[" + a + ", " + b + ", " + c + "]");
 }
 
-tripleFive();
-
+console.log(toArray(5, 2, 3));
